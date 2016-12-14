5034f504921e0befe44a62909b5db5a7208b8b28
norayr <norayr@arnet.am>
2016-12-14 19:09:36 +0400

    get remote and local file lists.

--- check_web_file_changes.sh
+++ check_web_file_changes.sh
@@ -1,11 +1,21 @@
 #set -x
 
 urls=`cat list.txt`
-files=""
-for i in $urllist
+
+remote_files=""
+local_files=""
+
+for i in $urls
 do
-file=`echo "${i##*/}"`
-files="$files $i"
+  file=`echo "${i##*/}"`
+  remote_files="$remote_files $file"
+  
+  file="${file%.*}"
+  local_files="$local_files $file"
 done
+
+echo $urls
+echo $remote_files
+echo $local_files
 
 
