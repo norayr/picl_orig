f3d4877e3d77e64a35fd77fa1dc8e546bd1541d0
norayr <norayr@arnet.am>
2016-12-14 17:03:44 +0400

    minimally ready

--- test.sh
+++ test.sh
@@ -16,4 +16,20 @@ echo "$pics $pics_md5"
 old_picl_md5=`md5sum $picl`
 old_pics_md5=`md5sum $pics`
 
+if [ "$picl_md5" != "$old_picl_md5" ]
+then
+ echo "$picl changed"
+ diff $picl ${picl}.txt
+else
+ echo "$picl unchanged"
+fi
+
+if [ "$pics_md5" != "$old_pics_md5" ]
+then
+ echo "$pics changed"
+ diff $pics ${pics}.txt
+else
+  eche "$pics unchanged"
+fi
+
 
